f788a7ee183ddb6750e3fa8f3223f21d7fd7a907
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-13 14:14:18 -0500

    now

--- migrations/20200512180244_cars_table.js
+++ migrations/20200512180244_cars_table.js
@@ -2,14 +2,14 @@ exports.up = function (knex) {
   return knex.schema.createTable("cars", (tbl) => {
     tbl.increments();
 
-    tbl.string("name", 255).notNullable().unique();
+    tbl.string("name", 100).notNullable().unique();
     tbl.string("vin", 17).notNullable().unique();
     tbl.string("make", 25).notNullable();
     tbl.string("model", 25).notNullable();
     tbl.integer("mileage", 15).notNullable();
 
-    tbl.string("transmission-type", 20).notNullable();
-    tbl.string("title-status", 15).notNullable();
+    tbl.string("transmission-type", 20);
+    tbl.string("title-status", 15);
 
     tbl.timestamps(true, true);
   });
